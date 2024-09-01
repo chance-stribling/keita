839cccc38b92c188309256f9534997d4e10e4e7b
chance-stribling <cstribling1717@gmail.com>
2024-09-01 09:59:00 -0400

    vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://main.d2w2a3j5paymhh.amplifyapp.com/',
   plugins: [
     vue(),
   ],
