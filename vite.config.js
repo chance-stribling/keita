a0f2effe46b65c3d6883daa2c540293d0e239005
chance-stribling <cstribling1717@gmail.com>
2024-09-01 09:54:30 -0400

    vite change

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://main.d2w2a3j5paymhh.amplifyapp.com/',
+  base: '/keita',
   plugins: [
     vue(),
   ],
